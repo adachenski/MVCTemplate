523d0fdab28b7aaec4c3e565197a42a51df65a95
adachenski <adachenski@aol.com>
2016-05-09 18:49:54 +0300

    Set up DbRepository with EF

--- data/MVCTemplate.Data/ApplicationDbContext.cs
+++ data/MVCTemplate.Data/ApplicationDbContext.cs
@@ -1,7 +1,11 @@
 ﻿namespace MVCTemplate.Data
 {
+    using Common.Models;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
@@ -13,6 +17,31 @@
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
         }
     }
 }
