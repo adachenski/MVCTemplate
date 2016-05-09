795186339293b0357f382d7b7ae88217ad25dcc6
adachenski <adachenski@aol.com>
2016-05-09 03:20:18 +0300

    Autofac installed and working

--- web/MVCTemplate.web/Controllers/HomeController.cs
+++ web/MVCTemplate.web/Controllers/HomeController.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using MVCTemplate.Data;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static MVCTemplate.Web.App_Start.AutofacConfig;
 
 namespace MVCTemplate.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IService service;
+
+
+        public HomeController(IService ser)
+        {
+            this.service = ser;
+        }
+
         public ActionResult Index()
         {
+            this.service.Work();
+            Trace.WriteLine("Nasko is in Home ");
+            var db = new ApplicationDbContext();
+            var userCount = db.Users.Count();
+
             return View();
         }
 
--- web/MVCTemplate.web/Global.asax.cs
+++ web/MVCTemplate.web/Global.asax.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace MVCTemplate.Web
+﻿namespace MVCTemplate.Web
 {
+    using MVCTemplate.Web.App_Start;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
